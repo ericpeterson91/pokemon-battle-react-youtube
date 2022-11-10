c9c5b89b0a6b8c55895a19609d068654ea914ca6
Eric Peterson <ericpeterson91@live.ca>
2022-11-10 16:50:12 -0700

    first

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import Pokemon from './Pokemon'
 
-function App() {
+const App = () => {
+
+  const [charizardHP, setCharizardHP] = useState(100)
+  const [blastoiseHP, setBlastoiseHP] = useState(100)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <Pokemon name='Charizard'/>
+      <Pokemon name='Blastoise'/>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/Pokemon.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Pokemon = ({name}) => {
+  return (
+    <div>{name}</div>
+  )
+}
+
+export default Pokemon
